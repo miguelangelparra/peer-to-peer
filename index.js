d83f4d02b3957d5bfce36193758dea2b6377e906
miguelangelparra <miguelangel.parradavila@gmail.com>
2020-07-05 23:31:11 -0300

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var port = process.env.PORT || 3000;
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// var Peer = require('simple-peer')
+// var wrtc = require('wrtc')
+ 
+// var peer1 = new Peer({ initiator: true, wrtc: wrtc })
+// var peer2 = new Peer({ wrtc: wrtc })
+
+// peer1.on("signal",(data)=>{
+// console.log(data)
+//   //document.getElementById("yourId").value =JSON.stringify(data)
+// })
+
+
+// http.listen(port, function(){
+//   console.log('listening on *:' + port);
+// });
+
